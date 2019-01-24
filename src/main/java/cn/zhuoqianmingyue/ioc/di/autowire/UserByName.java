d921f8c7a972976eee4689ec1b6119e54f9ece88
zhuoqianmingyue <ljk126wy@126.com>
2019-01-24 17:14:42 +0800

    添加Spring 自动装配相关示例程序

---
+++ src/main/java/cn/zhuoqianmingyue/ioc/di/autowire/UserByName.java
@@ -0,0 +1,13 @@
+package cn.zhuoqianmingyue.ioc.di.autowire;
+
+public class UserByName {
+	private Country country;
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+}
---
+++ src/main/java/cn/zhuoqianmingyue/ioc/di/autowire/UserByType.java
@@ -0,0 +1,12 @@
+package cn.zhuoqianmingyue.ioc.di.autowire;
+
+public class UserByType {
+	private Country country;
+
+	public Country getCountry() {
+		return country;
+	}
+	public void setACountry1(Country country) {
+		this.country = country;
+	}
+}
