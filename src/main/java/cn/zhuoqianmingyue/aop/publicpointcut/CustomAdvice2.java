9cb34edf09073b0e2a084aee30b2d322ed82c2db
zhuoqianmingyue <ljk126wy@126.com>
2019-02-20 17:22:49 +0800

    新增 Spring Aop 公共切入点的示例程序

---
+++ src/main/java/cn/zhuoqianmingyue/aop/publicpointcut/CustomAdvice2.java
@@ -0,0 +1,7 @@
+package cn.zhuoqianmingyue.aop.publicpointcut;
+
+public class CustomAdvice2 {
+	public void function() {
+		System.out.println("CustomAdvice2 .....");
+	}
+}
---
+++ src/main/java/cn/zhuoqianmingyue/aop/publicpointcut/User.java
@@ -0,0 +1,7 @@
+package cn.zhuoqianmingyue.aop.publicpointcut;
+
+public class User {
+	public void speak() {
+		System.out.println("speak.....");
+	}
+}
